f5465c78a5e1a257a80e003783464f6ea0f57aa4
amirwebset <amirrozy34@gmail.com>
2024-11-03 14:25:18 -0800

    Add files via upload

---
+++ tugas membuat kopi .py
@@ -0,0 +1,27 @@
+def buka_kemasan_kopi():
+    print("Buka kemasan kopi")
+
+def tuangkan_kopi_ke_cangkir():
+    print("Tuangkan kopi ke dalam cangkir")
+
+def tuangkan_gula_ke_cangkir():
+    print("Tuangkan gula ke dalam cangkir")
+
+def panaskan_air_hingga_mendidih():
+    print("Panaskan air hingga mendidih")
+
+def tuangkan_air_panas_ke_cangkir():
+    print("Tuangkan air panas ke dalam cangkir berisi kopi dan gula")
+
+def aduk_kopi_hingga_merata():
+    print("Aduk kopi hingga merata")
+
+# Mulai
+buka_kemasan_kopi()
+tuangkan_kopi_ke_cangkir()
+tuangkan_gula_ke_cangkir()
+panaskan_air_hingga_mendidih()
+tuangkan_air_panas_ke_cangkir()
+aduk_kopi_hingga_merata()
+# Selesai
+print("Selesai")
